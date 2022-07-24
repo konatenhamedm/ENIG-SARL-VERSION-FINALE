e5ea62e77436c3b3a4e64b34299d545067805745
konaten <konatenhamed@gmail.com>
2022-07-24 17:23:36 +0000

    first commit

---
+++ src/Form/ImagesType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Images;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImagesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('path', FileType::class, array(
+                "label" => false,
+                'required' => false,
+                'data_class' => null,
+                'mapped' => true,
+            ))
+            ->add('libelle', TextType::class, ["label" => false,])
+            /*->add('chambre')*/
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Images::class,
+        ]);
+    }
+}
